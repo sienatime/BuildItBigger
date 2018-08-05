82b12009c5a15e1d4b5b4cdc0adb5ebeb7ea1b4d
Siena Aguayo <siena.aguayo@gmail.com>
2018-08-05 15:54:54 -0700

    espresso test asserting joke is displayed

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -1,4 +1,26 @@
 package com.udacity.gradle.builditbigger;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+  @Rule
+  public ActivityTestRule<MainActivity> activityTestRule =
+      new ActivityTestRule<>(MainActivity.class);
+
+  @Test
+  public void openingApp_LoadsJoke() {
+    onView(withId(R.id.tell_joke_button)).perform(click());
+    onView(withId(R.id.text_view_joke)).check(matches(withText("Java is the joke!")));
+  }
 }
