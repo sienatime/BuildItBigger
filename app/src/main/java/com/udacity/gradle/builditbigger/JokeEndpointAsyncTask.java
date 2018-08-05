90d5b6023edce2efeab6f36d470e60ef6a34fa0c
Siena Aguayo <siena.aguayo@gmail.com>
2018-08-05 15:48:39 -0700

    changing asynctask to not have a reference to context

--- app/src/main/java/com/udacity/gradle/builditbigger/JokeEndpointAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeEndpointAsyncTask.java
@@ -1,53 +1,55 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
-import android.util.Pair;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 import java.io.IOException;
-import net.emojiparty.android.jokeactivity.JokeActivity;
 
-public class JokeEndpointAsyncTask extends AsyncTask<Context, Void, Pair<Context, String>> {
-  private static MyApi myApiService = null;
+public class JokeEndpointAsyncTask extends AsyncTask<Void, Void, String> {
+  private static MyApi myApi = null;
+  public interface OnJokeLoaded {
+    void success(String result);
+  }
+  private OnJokeLoaded callback;
+
+  JokeEndpointAsyncTask(OnJokeLoaded callback) {
+    this.callback = callback;
+  }
 
   @Override
-  protected Pair<Context, String> doInBackground(Context... params) {
-    if(myApiService == null) {  // Only do this once
-      MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-          new AndroidJsonFactory(), null)
-          // options for running against local devappserver
-          // - 10.0.2.2 is localhost's IP address in Android emulator
-          // - turn off compression when running against local devappserver
-          .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-          .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-            @Override
-            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws
-                IOException {
-              abstractGoogleClientRequest.setDisableGZipContent(true);
-            }
-          });
-      // end options for devappserver
-
-      myApiService = builder.build();
+  protected String doInBackground(Void... params) {
+    if(myApi == null) {
+      myApi = initializeApi();
     }
 
     try {
-      return new Pair<>(params[0], myApiService.tellJoke().execute().getData());
+      return myApi.tellJoke().execute().getData();
     } catch (IOException e) {
-      return new Pair<>(params[0], e.getMessage());
+      return e.getMessage();
     }
   }
 
   @Override
-  protected void onPostExecute(Pair<Context, String> result) {
-    Context context = result.first;
-    Intent jokeIntent = new Intent(context, JokeActivity.class);
-    jokeIntent.putExtra(JokeActivity.INTENT_JOKE_KEY, result.second);
-    context.startActivity(jokeIntent);
+  protected void onPostExecute(String result) {
+    callback.success(result);
+  }
+
+  private MyApi initializeApi() {
+    String emulatorLocalHost = "http://10.0.2.2:8080/";
+    MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+        new AndroidJsonFactory(), null)
+        .setRootUrl(emulatorLocalHost + "_ah/api/")
+        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+          @Override
+          public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws
+              IOException {
+            abstractGoogleClientRequest.setDisableGZipContent(true);
+          }
+        });
+
+    return builder.build();
   }
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,10 +1,13 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import net.emojiparty.android.jokeactivity.JokeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +37,15 @@ public class MainActivity extends AppCompatActivity {
   }
 
   public void tellJoke(View view) {
-    new JokeEndpointAsyncTask().execute(MainActivity.this);
+    JokeEndpointAsyncTask.OnJokeLoaded callback = new JokeEndpointAsyncTask.OnJokeLoaded() {
+      @Override public void success(String result) {
+        Context context = MainActivity.this;
+        Intent jokeIntent = new Intent(context, JokeActivity.class);
+        jokeIntent.putExtra(JokeActivity.INTENT_JOKE_KEY, result);
+        context.startActivity(jokeIntent);
+      }
+    };
+
+    new JokeEndpointAsyncTask(callback).execute();
   }
 }
